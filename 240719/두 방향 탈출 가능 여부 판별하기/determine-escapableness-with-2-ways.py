681e85960047d14ecc891641988daf0cbeb22964
YuJin Kwon <tokyj515@naver.com>
2024-07-19 20:52:03 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240719/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 240719/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -5,12 +5,13 @@ input = sys.stdin.readline
 n, m = map(int, input().split(" "))
 
 # 아래, 오른쪽
-dx = [1, 0]
-dy = [0, 1]
+dx = [0, 1]
+dy = [1, 0]
 
 graph = []
 visited = [[0 for _ in range(m)] for _ in range(n)]
 cnt = 0
+path = []
 
 for _ in range(n):
     temp = list(map(int, input().split()))
@@ -21,11 +22,11 @@ def dfs(x, y):
     visited[x][y] = 1
     global cnt
     cnt += 1
-    print(f"{x}, {y}")
+    # print(f"{x}, {y}")
+    path.append((x, y))
     
     if x == n-1 and y ==n-1:
-        print(1)
-        exit()
+        return
 
     for i in range(2):
         nx = x + dx[i]
@@ -36,10 +37,12 @@ def dfs(x, y):
                 dfs(nx, ny)
 
 
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1:
-            dfs(i, j)
+dfs(0, 0)
 
-print("cnt ", cnt)
-print(0)
+# print(cnt)
+# print(path)
+
+if (0, 0) in path and (n-1, n-1) in path:
+    print(1)
+else:
+    print(0)
