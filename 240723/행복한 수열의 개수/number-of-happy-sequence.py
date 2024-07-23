10b5dae4fa3b6b39d2e382caaa02e59ff631ae10
YuJin Kwon <tokyj515@naver.com>
2024-07-23 13:43:22 +0900

    Title: 행복한 수열의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240723/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,44 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split(" "))
+
+happy = 0
+graph = []
+
+for _ in range(n):
+    temp = list(map(int, input().split(" ")))
+    graph.append(temp)
+
+
+t_graph = list(map(list, zip(*graph)))
+
+
+if m == 1:
+    print(2*n)
+else:
+    for row in graph:
+        cnt = 1
+        for i in range(n-m+1):
+            if row[i] != row[i+1]:
+                continue
+            
+            cnt += 1
+
+            if cnt == m:
+                happy += 1
+        # print(f"row: {row} cnt: {cnt} happy: {happy}")
+
+    for row in t_graph:
+        cnt = 1
+        for i in range(n-m+1):
+            if row[i] != row[i+1]:
+                continue
+            
+            cnt += 1
+
+            if cnt == m:
+                happy += 1
+        # print(f"row: {row} cnt: {cnt} happy: {happy}")
+    print(happy)
