cd4a9ccb0ccb3410e9058a810f4582d55bcc63e0
YuJin Kwon <tokyj515@naver.com>
2024-07-23 14:07:06 +0900

    Title: 행복한 수열의 개수, Time: 158ms, Memory: 26MB, Status: Passed - CodeTree

--- 240723/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240723/행복한 수열의 개수/number-of-happy-sequence.py
@@ -4,7 +4,8 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split(" "))
 
-happy = 0
+# happy = 0
+happy = []
 graph = []
 
 for _ in range(n):
@@ -18,26 +19,38 @@ t_graph = list(map(list, zip(*graph)))
 if m == 1:
     print(2*n)
 else:
-    for row in graph:
-        cnt = 1
-        for i in range(n-m+1):
-            if row[i] != row[i+1]:
-                continue
+    # for row in graph:
+    #     cnt = 1
+    #     for i in range(n-m+1):
+    #         if row[i] != row[i+1]:
+    #             continue
             
-            cnt += 1
+    #         cnt += 1
 
-            if cnt == m:
-                happy += 1
+    #         if cnt == m:
+    #             happy += 1
+    #     print(f"row: {row} cnt: {cnt} happy: {happy}")
+    for row in graph:
+        for i in range(n-m+1):
+            cnt = 0
+            for k in range(i, i+m-1):
+                if row[k] == row[k+1]:
+                    cnt += 1
+            if cnt+1 == m:
+                # happy += 1 
+                happy.append(row)
+                continue   
         # print(f"row: {row} cnt: {cnt} happy: {happy}")
 
     for row in t_graph:
-        cnt = 0
-        for i in range(n-m+1):
-            if row[i] == row[i+1]:
-                cnt += 1
-
-            if cnt == m:
-                cnt = 0
-                happy += 1
+        for i in range(n-m+1): #열의 범위
+            cnt = 0
+            for k in range(i, i+m-1): # 열의 범위 내에서 움직이기
+                if row[k] == row[k+1]:
+                    cnt += 1
+            if cnt+1 == m:
+                # happy += 1 
+                happy.append(row)
+                continue
         # print(f"row: {row} cnt: {cnt} happy: {happy}")
-    print(happy)
+    print(len(list(set(tuple(h) for h in happy))))
