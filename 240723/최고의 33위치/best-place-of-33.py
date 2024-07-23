99a89fac984832c20d1c03f0f89f6e384a648b57
YuJin Kwon <tokyj515@naver.com>
2024-07-23 11:49:18 +0900

    Title: 최고의 33위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240723/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+graph = []
+# visited = [[0 for _ in range(n)] for _ in range(n)]
+cnt = 0
+max_val = 0
+
+
+for _ in range(n):
+    temp = list(map(int, input().split(" ")))
+    graph.append(temp)
+
+
+# for row in graph:
+#     print(row)
+
+
+def countOne(start_x, start_y):
+    cnt = 0
+    for i in range(start_x, start_x+3):
+        for j in range(start_y, start_y+3):
+            if graph[i][j] == 1:
+                cnt += 1
+
+    return cnt
+
+
+if n ==3:    
+    print(countOne(0, 0))
+
+else:
+    for i in range(n-2):
+        for j in range(n-2):
+            max_val = max(max_val, countOne(i, j))
+
+    print(max_val)
