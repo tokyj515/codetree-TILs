65ca4d37668c9c870e6a8737c096f3b2f6d9cec1
YuJin Kwon <tokyj515@naver.com>
2024-07-28 20:57:36 +0900

    Title: 십자 모양 폭발, Time: 138ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240728/십자 모양 폭발/cross-shape-bomb.py
@@ -0,0 +1,68 @@
+import sys
+
+
+input = sys.stdin.readline
+
+
+n = int(input())
+
+graph = []
+
+for _ in range(n):
+    temp = list(map(int, input().rstrip().split(" ")))
+    graph.append(temp)
+
+
+x, y = map(int, input().split(" "))
+
+# for row in graph:
+#     print(row)
+# print()
+
+
+
+def move():
+    t_graph = list(map(list, zip(*graph)))
+
+    result = []
+    
+    for row in t_graph:
+        temp = []
+        if row.count(0) != n:
+            cnt = row.count(0)
+
+            for _ in range(cnt):
+                temp.append(0)
+
+            for e in row:
+                if e != 0:
+                    temp.append(e)
+            result.append(temp)
+
+        else:
+            result.append(row)
+
+    return list(map(list, zip(*result)))
+
+
+
+
+scope = graph[x-1][y-1]
+
+if scope >= n:
+    for i in range(n):
+        graph[i][y-1] = 0
+        graph[x-1][i] = 0
+
+else:
+    # 가로
+    for i in range(y-2, y+1):
+        graph[x-1][i] = 0
+    # 세로
+    for i in range(x-2, x+1):
+        graph[i][y-1] = 0
+
+graph = move()
+
+for row in graph:
+    print(*row)
