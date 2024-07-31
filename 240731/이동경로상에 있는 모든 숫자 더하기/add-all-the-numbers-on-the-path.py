2e2487d8d1fd8367e2003053f9bdd35ff81fe59f
YuJin Kwon <tokyj515@naver.com>
2024-07-31 11:56:11 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240731/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -0,0 +1,48 @@
+import sys
+
+
+input = sys.stdin.readline
+
+
+n, t = map(int, input().split(" "))
+cmd = list(input().rstrip())
+
+graph = []
+
+for _ in range(n):
+    temp = list(map(int, input().split(" ")))
+    graph.append(temp)
+
+
+x = 0
+y = 0
+d = 0
+
+# N 0 -> E 1 -> S 2 -> W 3
+dx = [1, 0, -1, 0] 
+dy = [0, 1, 0, -1]
+
+cnt = 0
+
+# print(cmd)
+
+
+for c in cmd:
+    if c == "L":
+        d = (d+3) %4
+    elif c == "R":
+        d = (d+1) %4
+    elif c == "F":
+        nx = x + dx[d]
+        ny = y + dy[d]
+
+        if not (0<= nx and nx < n and 0<= ny and ny < n):
+            continue
+        
+        x = nx
+        y = ny
+        cnt += graph[x][y]
+
+    # print(f"{x} {y} => d: {d}, cnt: {cnt}")
+
+print(cnt)
