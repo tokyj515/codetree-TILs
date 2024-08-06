a271cec0c8fe32faebee2c892f474a859d9e8342
YuJin Kwon <tokyj515@naver.com>
2024-08-06 11:06:43 +0900

    Title: 문자에 따른 명령 2, Time: 162ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240806/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+
+
+
+
+# N, E, S, W
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+
+nx = 0
+ny = 0
+nd = 0
+
+order = list(input().rstrip())
+
+
+for o in order:
+    if o == 'L':    
+        nd = (nd+3)%4
+
+    elif o == 'R':
+        nd = (nd+1)%4
+
+    else:
+        nx = nx + dx[nd]
+        ny = ny + dy[nd]
+
+    # print(nx, ny, nd)
+
+print(nx, ny)
