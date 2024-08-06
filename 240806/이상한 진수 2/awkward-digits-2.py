7aad3f681a89e4ed3e8c7395500be233f27954ec
YuJin Kwon <tokyj515@naver.com>
2024-08-06 09:56:30 +0900

    Title: 이상한 진수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240806/이상한 진수 2/awkward-digits-2.py
+++ 240806/이상한 진수 2/awkward-digits-2.py
@@ -4,34 +4,48 @@ import sys
 input = sys.stdin.readline
 
 def natural(num):
-
     result = 0
     e = 1
     for n in num[::-1]:
         result += int(n)*e
         e *= 2
-    
-
     return result
+
+
+def isOriginList(origin, s):
+    for a ,b in zip(origin, s):
+        if a != b:
+            return False
+    return True
+
+
 
 s = list(input().rstrip())
 max_val = 0
+origin = s
 
-if len(s) == 1 and s[0] == '1':
-    print(0)
-    exit()
+if len(s) == 1:
+    if s[0] == '0':
+        print(1)
+    else:
+        print(0)
+    eixt()
+
 
 
 for i in range(len(s)):
     temp = s[i]
 
-    s[i] = '0'
+    
+    
+    s[i] = '0'x
     max_val = max(max_val, natural(''.join(s)))
-    # print(s)
+    print(s, max_val)
+
 
     s[i] = '1'
     max_val = max(max_val, natural(''.join(s)))
-    # print(s)
+    print(s, max_val)
 
 
     s[i] = temp
