36484c05b99c708281f0bf180d0107cc90f79aef
YuJin Kwon <tokyj515@naver.com>
2024-08-06 09:33:10 +0900

    Title: 이상한 진수 2, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240806/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,35 @@
+import sys
+
+
+input = sys.stdin.readline
+
+def natural(num):
+
+    result = 0
+    e = 1
+    for n in num[::-1]:
+        result += int(n)*e
+        e *= 2
+    
+
+    return result
+
+s = list(input().rstrip())
+max_val = 0
+
+for i in range(len(s)):
+    temp = s[i]
+
+    s[i] = '0'
+    max_val = max(max_val, natural(''.join(s)))
+    # print(s)
+
+    s[i] = '1'
+    max_val = max(max_val, natural(''.join(s)))
+    # print(s)
+
+
+    s[i] = temp
+
+
+print(max_val)
