ba2f6131b822b0d5bf105cf4f155e0c19f1c587f
YuJin Kwon <tokyj515@naver.com>
2024-08-06 11:07:59 +0900

    Title: 되돌아오기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240806/되돌아오기/come-back.py
@@ -0,0 +1,38 @@
+import sys
+
+
+input = sys.stdin.readline
+
+
+start_x = 0
+start_y = 0
+x = 0 
+y = 0
+
+direct = {"N": 0, "E": 1, "S": 2, "W": 3}
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+
+
+n = int(input())
+cnt = 0
+
+for _ in range(n):
+    di, move = input().rstrip().split(" ")
+    move = int(move)
+
+    for m in range(move):
+        x = x + dx[direct[di]]
+        y = y + dy[direct[di]]
+        cnt += 1
+
+        if x == 0 and y == 0:
+            print(cnt)
+            exit()
+
+    # print(cnt, "-", x, y)
+
+
+print(-1)
