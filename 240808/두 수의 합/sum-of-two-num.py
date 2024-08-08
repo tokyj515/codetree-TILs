47af4e3dfa10d4262431f5b4966cd00a98c89594
YuJin Kwon <tokyj515@naver.com>
2024-08-08 15:47:54 +0900

    Title: 두 수의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240808/두 수의 합/sum-of-two-num.py
@@ -0,0 +1,44 @@
+import sys
+from collections import Counter
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+num = list(map(int, input().split()))
+
+num.sort()
+
+counter = dict(Counter(num))
+cnt = 0
+
+
+for i in range(len(num)):
+
+    a = num[i]
+
+    if k-a in counter.keys():
+        b  = k-a
+    else:
+        continue
+    
+
+    if counter[a] == counter[b]:
+        cnt += counter[a]
+        counter.pop(a)
+        counter.pop(b)
+    elif counter[a] > counter[b]:
+        cnt += counter[b]
+        counter[a] -= counter[b]
+        counter.pop(b)
+    elif counter[a] < counter[b]:
+        cnt += counter[a]
+        counter[b] -= counter[a]
+        counter.pop(a)
+    
+    # print(cnt)
+    # print(counter)
+    # print()
+
+
+print(cnt)
