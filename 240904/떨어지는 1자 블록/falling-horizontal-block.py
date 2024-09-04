7d12eecf3d97ce0470afa07ccec48a9059d0b061
YuJin Kwon <tokyj515@naver.com>
2024-09-04 18:16:08 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240904/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,26 @@
+import sys
+
+
+input = sys.stdin.readline
+
+
+n, m, k = map(int, input().split(" "))
+
+graph = []
+
+for _ in range(n):
+    temp = list(map(int, input().split(" ")))
+    graph.append(temp)
+
+
+# k열부터 포함해서 m칸
+
+# for row in graph:
+#     print(row)
+
+
+for i in range(k-1, k+m):
+    graph[k][i] = 1
+
+for row in graph:
+    print(*row)
