71bc02e40561c9bc75de013bf9d377ee919da19a
YuJin Kwon <tokyj515@naver.com>
2024-09-05 11:50:30 +0900

    Title: 아름다운 수, Time: 287ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 240905/아름다운 수/beautiful-number.py
@@ -0,0 +1,43 @@
+import sys
+import copy
+
+input = sys.stdin.readline
+
+result = []
+answer = []
+num = [1, 2, 3, 4]
+
+n = int(input())
+
+
+def backtrack(dep):
+    global n
+
+    if dep == n:
+        temp = copy.deepcopy(answer)
+        result.append(''.join(map(str, temp)))
+        return
+
+    for i in range(4):
+        answer.append(num[i])
+
+        backtrack(dep+1)
+
+        answer.pop()
+
+
+backtrack(0)
+
+# print(result)
+
+cnt = 0
+for r in result:
+    origin = r
+
+    r = r.replace('4444', '').replace('333', '').replace('22', "").replace('1', '')
+
+    if r == '':
+        # print(origin)
+        cnt += 1
+
+print(cnt)
