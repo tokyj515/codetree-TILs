d374fc9fe09843a0cd5c75bca47aa67905c3d58f
YuJin Kwon <tokyj515@naver.com>
2024-09-12 19:26:17 +0900

    Title: 거꾸로 순열, Time: 262ms, Memory: 33MB, Status: Passed - CodeTree

---
+++ 240912/거꾸로 순열/backward-permutation.py
@@ -0,0 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+num = [i for i in range(n, 0, -1)]
+visited = [0 for _ in range(n)]
+
+answer = []
+result = []
+
+
+def backtrack(dep):
+    if dep == n:
+        temp = answer[::]
+        result.append(temp)
+        return
+
+    for i in range(n):
+        if not visited[i]:
+            answer.append(num[i])
+            visited[i] = 1
+
+            backtrack(dep+1)
+
+            answer.pop()
+            visited[i] = 0
+
+backtrack(0)
+
+for res in result:
+    print(*res)
