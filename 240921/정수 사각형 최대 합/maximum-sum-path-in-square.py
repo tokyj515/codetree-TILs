b5bdb8cc2f2dbd491739818ed817f029de11477f
YuJin Kwon <tokyj515@naver.com>
2024-09-21 22:22:37 +0900

    Title: 정수 사각형 최대 합, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240921/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -0,0 +1,32 @@
+n = int(input())
+
+graph = []
+
+dp= [[0 for _ in range(n)] for _ in range(n)]
+
+
+
+
+for _ in range(n):
+    temp = list(map(int, input().split(" ")))
+    graph.append(temp)
+
+
+dp[0][0] = graph[0][0]
+for i in range(1, n):
+    dp[0][i] += dp[0][i-1] + graph[0][i]
+    dp[i][0] += dp[i-1][0] + graph[i][0]
+
+
+# for row in dp:
+#     print(row)
+
+
+
+
+for i in range(1, n):
+    for j in range(1, n):
+        dp[i][j] = max(dp[i][j-1], dp[i-1][j-1]) + graph[i][j]
+
+
+print(dp[n-1][n-1])
