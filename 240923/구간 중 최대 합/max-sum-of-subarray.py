dbc7bf8d2e3e3246826860f376564fbe2bb949a2
YuJin Kwon <tokyj515@naver.com>
2024-09-23 23:52:03 +0900

    Title: 구간 중 최대 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240923/구간 중 최대 합/max-sum-of-subarray.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split(" "))
+num = list(map(int, input().split(" ")))
+
+sum = 0
+
+
+for i in range(n-k+1):
+    temp = 0
+    for j in range(i, i+k):
+        temp += num[j]
+    sum = max(sum, temp)
+
+
+print(sum)
