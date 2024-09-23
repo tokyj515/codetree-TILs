f711a091e20feb12ac8b072e4f1f8190fcd517da
YuJin Kwon <tokyj515@naver.com>
2024-09-23 23:47:15 +0900

    Title: 특정 수와 근접한 합, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 240923/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240923/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -7,14 +7,16 @@ num = list(map(int, input().rstrip().split(" ")))
 
 # S에서 토탈값을 뺀 것이 최소인 경우
 min_val = 1000000000000
+total = sum(num)
 # sum = 0
 
 
-permu_list = list(permutations(num, n-2))
+permu_list = list(permutations(num, 2))
 
 for permu in permu_list:
 
-    temp = abs(s- sum(permu))
+    # temp = abs(s- sum(permu))
+    temp = abs(s - (total - sum(permu)))
 
 
     min_val = min(min_val, temp)
