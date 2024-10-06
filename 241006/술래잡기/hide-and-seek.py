ca7b5cac0e732ea0b0344cf9c4e4cb2faf24c2d2
YuJin Kwon <tokyj515@naver.com>
2024-10-06 18:57:46 +0900

    Title: 술래잡기, Time: 203ms, Memory: 26MB, Status: Passed - CodeTree

--- 241006/술래잡기/hide-and-seek.py
+++ 241006/술래잡기/hide-and-seek.py
@@ -1,134 +1,71 @@
-# 11:30
-# 술래: 중앙에서 시작
-# 도망자
-# 좌우: 항상 오른쪽 보고 시작
-# 상하: 항상 아래쪽 보고 시작
+n, m, h, K = map(int, input().split())
 
-# 1턴: 술래 -> 도망자
-# 술래와 도망자 거리: |x1 - x2| + |y1 - y2|
-# 술래와의 거리가 3이하인 도망자는 이동
-    # 다음 칸에 술래가 있는 게 아니면 이동 가능
-    # 격자에서 넘치는 경우 -> 방향 전환 후 술래가 없다면 이동
-
-# 술래는 이동 후에 해당 방향으로 바라보기
-    # 이때 나무가 있는 곳은 못 잡고
-    # 나무가 없다면 도망자를 잡음
-# n번째 턴 * m명 잡으면 이게 해당 스테이지에서 점수
-
-
-# =======================================================================================
-n, m , h, K = map(int, input().split(" "))
-
+# 도망자 좌표 입력
 runners = []
 for _ in range(m):
-    temp = list(map(int, input().split(" ")))
-    runners.append(temp)
+    runners.append(list(map(int, input().split())))
 
+# 나무좌표 입력
 trees = set()
 for _ in range(h):
-    i, j = map(int, input().split(" "))
+    i, j = map(int, input().split())
     trees.add((i, j))
 
-graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
-
-# =======================================================================================
-# 술래 변수
-# 상 우 하 좌
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-M = (n+1)//2
-x, y, d = M, M, 0
-max_dist, cnt, flag, direct = 1, 0, 0, 1
-
-
-# 도망자 변수
-opp = {0:1, 1:0, 2:3, 3:2}
+# 0(좌) 1(우) 2(하) 3(상)
 rx = [0, 0, 1, -1]
 ry = [-1, 1, 0, 0]
+opp = {0: 1, 1: 0, 2: 3, 3: 2}  # 반대 방향
 
+# 방향: 상, 우, 하, 좌 (술래 방향, 바깥쪽으로 돌 때의 방향)
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
-# =======================================================================================
+max_dist, cnt, flag, direct = 1, 0, 0, 1
+M = (n + 1) // 2
+x, y, d = M, M, 0
+
 answer = 0
+for k in range(1, K + 1):  # K턴만큼 게임 진행
+    # [1] 도망자의 이동
+    for i in range(len(runners)):
+        if abs(runners[i][0] - x) + abs(runners[i][1] - y) <= 3:  # 술래와 거리 3 이하인 경우 이동
+            nx, ny = runners[i][0] + rx[runners[i][2]], runners[i][1] + ry[runners[i][2]]
+            if 1 <= nx <= n and 1 <= ny <= n:  # 범위 내면 술래 체크
+                if (nx, ny) != (x, y):  # 술래 위치가 아니면 이동
+                    runners[i][0], runners[i][1] = nx, ny
+            else:  # 범위 밖이면 방향 반대
+                runners[i][2] = opp[runners[i][2]]  # 반대 방향 전환 및 저장
+                nx, ny = runners[i][0] + rx[runners[i][2]], runners[i][1] + ry[runners[i][2]]
+                if (nx, ny) != (x, y):
+                    runners[i][0], runners[i][1] = nx, ny
 
-for k in range(1, K+1):
-
-    # 1. 도망자 이동
-    for runner in runners:
-        # 술래: x, y
-        if abs(x - runner[0]) + abs(y-runner[1]) <= 3:
-            rd = runner[2]
-            #다음으로 이동할 좌표
-            rnx = runner[0] + rx[rd]
-            rny = runner[1] + ry[rd]
-
-            # 도망자의 다음 좌표 범위 확인
-            if 1<= rnx and rnx <= n and 1<= rny and rny <= n:
-                if (rnx, rny) != (x, y):
-                    runner[0] = rnx
-                    runner[1] = rny
-                else:
-                    nd = opp[runner[2]]
-                    rnx = runner[0] + rx[nd]
-                    rny = runner[1] + ry[nd]
-
-                    # 방향을 바꾸고 이동하고 싶은 곳이 술래가 아닌지 한 번 더 확인
-                    if (rnx, rny) != (x, y):
-                        runner[0] = rnx
-                        runner[1] = rny
-                        runner[2] = nd # 방향도 바뀌었으니까
-            
-  
-    # 2. 술래 이동    
-    graph[x][y] = k
+    # [2] 술래의 이동
     cnt += 1
-
-    x = x + dx[d]
-    y = y + dy[d]
-
-    if (x, y) == (1, 1):
+    x, y = x + dx[d], y + dy[d]
+    if (x, y) == (1, 1):  # 안쪽으로 동작하는 달팽이
         max_dist, cnt, flag, direct = n, 1, 1, -1
-        d = 2
-
-
-    elif (x, y) == (M, M):
+        d = 2  # 초기 방향은 아래(하)
+    elif (x, y) == (M, M):  # 바깥쪽으로 동작하는 달팽이
         max_dist, cnt, flag, direct = 1, 0, 0, 1
         d = 0
-
     else:
-        if max_dist == cnt:
+        if cnt == max_dist:  # 방향 변경
             cnt = 0
             d = (d + direct) % 4
-
-            if flag:
-                max_dist += direct
-                flag = 0
-            else:
+            if flag == 0:
                 flag = 1
+            else:
+                flag = 0  # 두 번에 한 번씩 길이 증가
+                max_dist += direct
 
-
-
-    # 3. 술래가 도망자 잡기
-    # 술래가 현재 바라보는 방향에서 자기 포함 3칸에 도망자가 있는지 확인
-    # 나무가 없는 도망자
-    # 도망자를 탐색할 땐 맨 뒤에서부터 해야지 인덱스 관리를 안 할 수 있음
-    catcher_set = ((x, y), (x+dx[d], y+dy[d]), (x+dx[d]*2, y+dy[d]*2))
-    
-
-    for i in range(len(runners)-1, -1, -1):
-        runner = runners[i]
-        if (runner[0], runner[1]) in catcher_set and (runner[0], runner[1]) not in trees:
+    # [3] 도망자 잡기 (술래 자리 포함 3칸: 나무가 없는 도망자면 잡힘)
+    catcher_set = set(((x, y), (x + dx[d], y + dy[d]), (x + dx[d] * 2, y + dy[d] * 2)))
+    for i in range(len(runners) - 1, -1, -1):
+        if (runners[i][0], runners[i][1]) in catcher_set and (runners[i][0], runners[i][1]) not in trees:
             runners.pop(i)
             answer += k
 
-    
-
-    # 4. 도망자가 없다면? 점수도 없음 -> 술래만 돌고 있음
-    # if not runner:
-    #     break
-
-    # for row in graph:
-    #     print(row)
-    # print(runners)
-
-
+    # 도망자가 없다면 더 이상 점수도 없음
+    if not runners:
+        break
 print(answer)
